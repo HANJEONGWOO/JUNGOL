d90412183240efc7607a447b0dea96b2e384e132
HANJEONGWOO <hotgkswjddn@naver.com>
2016-07-17 23:14:42 +0900

    160717

--- 1278.cpp
+++ 1278.cpp
@@ -1,12 +1,14 @@
-#include <stdio.h>
 
+#include <stdio.h>
 
 int g_weight[1100];
 int g_value[1100];
 
 int N, W;
 
-int DT[1100][1100];
+int DT[1100][11000];
+
+int mymax(int a, int b);
 
 int main(void)
 {
@@ -21,25 +23,22 @@ int main(void)
 	{
 		for (int j = 1; j <= W; j++)
 		{
-			if (g_weight[i] <= j && DT[i - 1][j] < DT[i - 1][j - g_weight[i]] + g_value[i])
-				DT[i][j] = DT[i - 1][j - g_weight[i]] + g_value[i];
-			else
+			if (g_weight[i] > j)
 				DT[i][j] = DT[i - 1][j];
+			else
+				DT[i][j] = mymax(DT[i - 1][j], DT[i - 1][j - g_weight[i]] + g_value[i]);
 		}
 	}
-
-	/*
-	for (int i = 1; i <= N; i++)
-	{
-	for (int j = 1; j <= W; j++)
-	{
-	printf("%d ", DT[i][j]);
-	}
-	printf("\n");
-	}
-	*/
 
 	printf("%d\n", DT[N][W]);
 
 	return 0;
+}
+
+int mymax(int a, int b)
+{
+	if (a >= b)
+		return a;
+	else
+		return b;
 }
